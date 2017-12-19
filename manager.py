7a11681be623c190b8728858f7086244172173f6
Despair <despair@gmail.com>
2017-12-19 15:30:01 +0800

    逻辑代码和运行文件分开

--- manager.py
+++ manager.py
@@ -1,22 +1,10 @@
 # -*- coding:utf-8 -*-
-from flask import Flask,session
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
-import redis
-from flask_wtf.csrf import CSRFProtect
-from flask_session import Session
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import Config
-
-
-app = Flask(__name__)
-
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-csrf = CSRFProtect(app)
-Session(app)
+from i_home import app, db
 
 manager = Manager(app)
 Migrate(app, db)
