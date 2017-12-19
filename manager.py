07a9a3f5d19b787eb1c41408cfb6ced054591f79
Despair <despair@gmail.com>
2017-12-19 15:26:03 +0800

    抽取配置代码到配置模块

--- manager.py
+++ manager.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
+from flask import Flask,session
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf.csrf import CSRFProtect
@@ -12,14 +12,15 @@ from config import Config
 app = Flask(__name__)
 
 app.config.from_object(Config)
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-csrf = CSRFProtect
-Session(app)
 db = SQLAlchemy(app)
+
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+csrf = CSRFProtect(app)
+Session(app)
 
 manager = Manager(app)
 Migrate(app, db)
-manager.add_command(db, MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route("/")
@@ -28,5 +29,45 @@ def index():
 
 
 if __name__ == '__main__':
-    # manager.run()
-    app.run()
+    manager.run()
+    # app.run()
+
+# -*- coding:utf-8 -*-
+#
+# import redis
+# from flask import Flask, session
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_wtf.csrf import CSRFProtect
+# from flask_session import Session
+# from flask_script import Manager
+# from flask_migrate import Migrate, MigrateCommand
+# from config import Config
+#
+#
+# app = Flask(__name__)
+# app.config.from_object(Config)
+#
+# # 初始化数据库连接
+# db = SQLAlchemy(app)
+# # 初始化redis
+# redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# # 开启CSRF保护
+# csrf = CSRFProtect(app)
+# # 给当前app的session设置保存路径
+# Session(app)
+#
+# manager = Manager(app)
+# # 集成数据库的迁移
+# Migrate(app, db)
+# manager.add_command('db', MigrateCommand)
+#
+#
+# @app.route("/index", methods=["GET", "POST"])
+# def index():
+#     session["name"] = "xiaohua"
+#     redis_store.set('name', 'xiaofang')
+#     return "index"
+#
+#
+# if __name__ == '__main__':
+#     manager.run()
