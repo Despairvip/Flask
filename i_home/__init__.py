8d1f9cce2dbc6fc2cc91841406f853659d46e642
Despair <despair@gmail.com>
2017-12-20 09:16:50 +0800

    添加静态文件

--- i_home/__init__.py
+++ i_home/__init__.py
@@ -7,7 +7,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
-
 from config import config_dict
 
 # 数据库
--- manager.py
+++ manager.py
@@ -3,6 +3,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from i_home import create_app, db
+from i_home import models
 
 app = create_app("development")
 manager = Manager(app)
