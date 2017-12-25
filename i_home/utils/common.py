9d32196226e0fcfff2288e1473dab459a28f3aae
Despair <despair@gmail.com>
2017-12-25 15:11:33 +0800

    创建检测登录装饰器

--- i_home/utils/common.py
+++ i_home/utils/common.py
@@ -1,6 +1,11 @@
 # -*- coding:utf-8 -*-
+import functools
 
+from flask import g
+from flask import session, jsonify
 from werkzeug.routing import BaseConverter
+
+from i_home.utils.response_code import RET
 
 
 class RegexConverter(BaseConverter):
@@ -8,3 +13,16 @@ class RegexConverter(BaseConverter):
     def __init__(self, url_map, *args):
         super(RegexConverter, self).__init__(url_map)
         self.regex = args[0]
+
+
+def login_session_check(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        user_id = session.get("user_id")
+        if not user_id:
+            return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+        else:
+            g.user_id = user_id
+            return f(*args, **kwargs)
+
+    return wrapper
