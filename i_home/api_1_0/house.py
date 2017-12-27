7d7045b4c1367ddf08739de44386107df37b5379
Despair <despair@gmail.com>
2017-12-27 14:48:58 +0800

    添加房源信息

--- i_home/api_1_0/house.py
+++ i_home/api_1_0/house.py
@@ -5,14 +5,56 @@ from flask import request
 
 from i_home import redis_store, db
 from i_home.api_1_0 import api
-from i_home.constants import AREA_INFO_REDIS_EXPIRES
-from i_home.models import Area, House, Facility
+from i_home.constants import AREA_INFO_REDIS_EXPIRES, QINIU_DOMIN_PREFIX
+from i_home.models import Area, House, Facility, HouseImage
+from i_home.utils.image_storage import storage_image
 from i_home.utils.common import login_session_check
 from i_home.utils.response_code import RET
 
 
+@api.route("/houses/<int:house_id>/images", methods=['POST'])
+@login_session_check
+def release_house_images(house_id):
+    try:
+        images_file = request.files.get("house_image").read()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DATAERR, errmsg="获取参数失败")
+
+    try:
+        house = House.query.get(house_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="获取房屋信息失败")
+
+    if not house:
+        return jsonify(errno=RET.DATAERR, errmsg="房屋不存在")
+
+    try:
+        url = storage_image(images_file)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="上传失败")
+
+    if not house.index_image_url:
+        house.index_image_url = url
+
+    house_image = HouseImage()
+    house_image.house_id = house_id
+    house_image.url = url
+
+    try:
+        db.session.add(house_image)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="存储失败")
+    return jsonify(errno=RET.OK, errmsg="保存成功", data={'url': QINIU_DOMIN_PREFIX + url})
+
+
 @api.route("/areas")
-def get_areas():
+def release_areas():
     # 城区信息缓存到redis中
     try:
         areas_dict = redis_store.get('area_info')
@@ -44,38 +86,38 @@ def get_areas():
 @login_session_check
 def get_houses_info():
     user_id = g.user_id
-    data = request.json
-    title = data.get('title')
-    price = data.get('price')
-    area_id = data.get('area_id')
-    address = data.get('address')
-    room_count = data.get('title')
-    acreage = data.get('acreage')
-    unit = data.get('unit')
-    capacity = data.get('capacity')
-    beds = data.get('beds')
-    deposit = data.get('deposit')
-    min_days = data.get('min_days')
-    max_days = data.get('max_days')
-    facility = data.get('facility')
+
+    json_dict = request.json
+    title = json_dict.get('title')
+    price = json_dict.get('price')
+    address = json_dict.get('address')
+    area_id = json_dict.get('area_id')
+    room_count = json_dict.get('room_count')
+    acreage = json_dict.get('acreage')
+    unit = json_dict.get('unit')
+    capacity = json_dict.get('capacity')
+    beds = json_dict.get('beds')
+    deposit = json_dict.get('deposit')
+    min_days = json_dict.get('min_days')
+    max_days = json_dict.get('max_days')
 
     if not all([title, price, area_id, address, room_count, acreage, unit, capacity, beds, deposit,
-                min_days, max_days, facility]):
+                min_days, max_days]):
         return jsonify(errno=RET.DATAERR, errmsg="参数不正确")
 
     # 转换price的单位
     try:
-        price = int(float(price)*100)
-        deposit = int(float(deposit)*100)
+        price = int(float(price) * 100)
+        deposit = int(float(deposit) * 100)
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DATAERR, errmsg="参数有误")
 
     house = House()
     house.user_id = user_id
+    house.area_id = area_id
     house.title = title
     house.price = price
-    house.area_id = price
     house.address = address
     house.room_count = room_count
     house.acreage = acreage
@@ -86,6 +128,7 @@ def get_houses_info():
     house.min_days = min_days
     house.max_days = max_days
 
+    facility = json_dict.get('facility')
     if facility:
         house.facilities = Facility.query.filter(Facility.id.in_(facility)).all()
 
@@ -97,5 +140,3 @@ def get_houses_info():
         db.session.rollback()
         return jsonify(errno=RET.DBERR, errmsg="存储失败")
     return jsonify(errno=RET.OK, errmsg="发布成功", data={'house_id': house.id})
-
-
